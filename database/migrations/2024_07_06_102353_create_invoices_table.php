0a2162d3cbfabcdce3ae3b19d71671a32db8cd06
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-07-06 18:00:11 +0700

    update: make models and migrate to database

---
+++ database/migrations/2024_07_06_102353_create_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->constrained('projects')->onDelete('cascade');
+            $table->string('title');
+            $table->text('detail')->nullable();
+            $table->text('notes')->nullable();
+            $table->integer('total_price');
+            $table->date('due_date');
+            $table->date('issue_date');
+            $table->date('paid_date')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
